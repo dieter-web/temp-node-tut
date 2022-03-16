25330899cb9ff40895dc4178103362d157b0f023
Dieter Krause <dieterkrause31960@gmail.com>
2022-03-16 13:48:05 +0100

    first commit

---
+++ 12-http.js
@@ -0,0 +1,16 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  // console.log(req)
+  if (req.url === '/') {
+    res.end('Welcome to our home page')
+  }
+  if (req.url === '/about') {
+    res.end('Here is our short history')
+  }
+  if (req.url !== '/' && req.url !== '/about') {
+    res.end(`<h1>Oops!</h1> <p>We can't seem to find the page you are looking for</p>
+    <a href="/">back home</a>`)
+  }
+})
+server.listen(3000)
